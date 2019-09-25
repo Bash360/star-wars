8b1adec2cb4ca870ca249f1e640bb502a99ad8a2
mark bashir <beejayphil@gmail.com>
2019-09-25 20:26:25 +0100

    imported all resource

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -4,13 +4,16 @@ import style from './home.module.css';
 import axios from 'axios';
 import GenericButton from '../generic-button';
 import { NavLink } from 'react-router-dom';
+import { promised } from 'q';
 
 function Home() {
   const [starships, setStarships] = useState([]);
   const [planets, setPlanets] = useState([]);
+  const [people, setPeople] = useState([]);
   const [loading, setLoading] = useState(true);
   const [starshipImages, setStarshipImages] = useState([]);
   const [planetImages, setPlanetImages] = useState([]);
+  const [peopleImages, setPeopleImages] = useState([]);
   let starshipCards = [];
   const fetchResources = async () => {
     const resources = await Promise.all([
@@ -20,6 +23,8 @@ function Home() {
     ]);
 
     setStarships(resources[0].data.results);
+    setPlanets(resources[1].data.results);
+    setPeople(resources[2].data.results);
     let starshipImages = await Promise.all([
       import('../../assets/starship-1.jpg'),
       import('../../assets/starship-2.jpg'),
@@ -33,8 +38,16 @@ function Home() {
       import('../../assets/planet-2.jpg'),
       import('../../assets/planet-3.jpg'),
     ]);
+    let peopleImages = await Promise.all([
+      import('../../assets/character-1.jpg'),
+      import('../../assets/character-2.jpg'),
+      import('../../assets/character-3.jpg'),
+      import('../../assets/character-4.jpg'),
+    ]);
     setStarshipImages(starshipImages);
     setPlanetImages(planetImages);
+    setPeopleImages(peopleImages);
+
     setLoading(false);
   };
   useEffect(() => {
