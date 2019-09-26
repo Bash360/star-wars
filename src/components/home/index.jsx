ce8331c239ec37e0a4b3b5a037e032a5f533f86e
mark bashir <beejayphil@gmail.com>
2019-09-26 07:14:57 +0100

    added people cards

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import Card from '../starship-card';
+import StarshipCard from '../starship-card';
 import style from './home.module.css';
 import axios from 'axios';
 import GenericButton from '../generic-button';
 import { NavLink } from 'react-router-dom';
-import { promised } from 'q';
+import PlanetCard from '../planet-card';
+import PeopleCard from '../people-card';
 
 function Home() {
   const [starships, setStarships] = useState([]);
@@ -15,6 +16,8 @@ function Home() {
   const [planetImages, setPlanetImages] = useState([]);
   const [peopleImages, setPeopleImages] = useState([]);
   let starshipCards = [];
+  let planetCards = [];
+  let peopleCards = [];
   const fetchResources = async () => {
     const resources = await Promise.all([
       axios.get('https://swapi.co/api/starships?page=1'),
@@ -25,6 +28,7 @@ function Home() {
     setStarships(resources[0].data.results);
     setPlanets(resources[1].data.results);
     setPeople(resources[2].data.results);
+    console.log(resources[2].data.results);
     let starshipImages = await Promise.all([
       import('../../assets/starship-1.jpg'),
       import('../../assets/starship-2.jpg'),
@@ -55,12 +59,14 @@ function Home() {
   }, []);
 
   if (!loading) {
+    let peopleCount = 0;
     for (let index = 0; index < 6; index++) {
       let { url, model, cargo_capacity, name } = starships[index];
       let random = Math.floor(Math.random() * 5);
+      let planetRandom = Math.floor(Math.random() * 2);
       starshipCards.push(
         <li key={url}>
-          <Card
+          <StarshipCard
             model={model}
             capacity={cargo_capacity}
             src={starshipImages[random].default}
@@ -69,6 +75,28 @@ function Home() {
           />
         </li>,
       );
+
+      planetCards.push(
+        <li key={planets[index].url}>
+          <PlanetCard
+            src={planetImages[planetRandom].default}
+            name={planets[index].name}
+            alternate="planet"
+          ></PlanetCard>
+        </li>,
+      );
+      if (peopleCount <= 3) {
+        peopleCards.push(
+          <li key={people[index].url}>
+            <PeopleCard
+              src={peopleImages[peopleCount].default}
+              name={people[index].name}
+              alternate="people"
+            />
+          </li>,
+        );
+        peopleCount += 1;
+      }
     }
   }
 
@@ -84,6 +112,17 @@ function Home() {
       </NavLink>
       <h3 className={style.planets__header}>Popular Planets</h3>
       <hr />
+      <div className={style.planets}>
+        <ul>{planetCards}</ul>
+      </div>
+      <h3 className={style.people__header}>Popular Characters</h3>
+      <hr />
+      <div className={style.people}>
+        <ul>{peopleCards}</ul>
+      </div>
+      <NavLink className={style.home__link} to="/">
+        <GenericButton text="View More" />
+      </NavLink>
     </React.Fragment>
   ) : (
     <div className={style.spinner}>
