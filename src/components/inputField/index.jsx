40eccbac44ac1b8edd6f9253471e1cf85719da68
mark bashir <beejayphil@gmail.com>
2019-09-27 18:32:27 +0100

    added function to ahndle onchange event

--- src/components/inputField/index.jsx
+++ src/components/inputField/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import style from './input.module.css';
 import PropTypes from 'prop-types';
-function InputField({ type, placeholder }) {
+function InputField({ type, placeholder, change }) {
   return (
     <input
+      onChange={change}
       required
       className={style.input}
       type={type}
