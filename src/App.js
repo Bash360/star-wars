33a1e19051998c93b3d18a00a54a292519feb199
mark bashir <beejayphil@gmail.com>
2019-09-29 18:13:24 +0100

    removed 404 route

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './pages/home';
 import imageContext from './imageContext';
 import StarshipPage from './pages/starships';
-import Four0Four from './pages/404';
 import Characters from './pages/characters';
 import searchContext from './searchContext';
 import Character from './pages/character';
